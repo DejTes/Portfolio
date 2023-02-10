fecf80effb39c105a96f51ccb637db3af4683b7d
dejtes <dejenaref@gmail.com>
2023-02-10 17:13:16 -0500

    Wrapping up with styling

---
+++ script.js
@@ -0,0 +1,48 @@
+const tablinks = document.getElementsByClassName("tab-links");
+const tabcontents = document.getElementsByClassName("tab-contents");
+
+
+// side menu
+// $(() => {});
+
+
+
+
+
+
+  // let sidemenu = document.getElementById('sidemenu');
+  // function openmenu() {
+  //     sidemenu.style.right = '0';
+
+  // }
+  // function closemenu() {
+  //     sidemenu.style.right = '-250px';
+      
+  // }
+
+
+
+
+
+
+//<script>
+
+// let openBtn = document.querySelector(".fa-xmark");
+// let closeBtn = document.querySelector(".fa-xmark");
+// let sidemenu = document.querySelector(".sidemenu");
+
+// openBtn.addEventListener('onclick', function(){
+//     sidemenu.style.display = 'block';
+// });
+// closeBtn.addEventListener('onclick', function(){
+//     sidemenu.style.display = '-none';
+// });
+
+
+
+// </script>
+
+
+
+
+
