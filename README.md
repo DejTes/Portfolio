faa7b8086b4c359d2449d346bcb3d40e85ddd25e
dejtes <dejenaref@gmail.com>
2023-02-12 08:35:06 -0500

    Readme file updated and almost wrapping up with the fainal draft of the portfolio

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-Setting up portfolio!!!
+
+
+# Personal Portfolio
+This is my personal portfolio.
+
+# Live Demo
+you can check a live demo of the portfolio [here](https://dejtes-portfolio.netlify.app/#header)
+
+## _Technologies used_
+####  The following technologies were used to develop this portfolio
+---
+---
+- HTML
+- CSS
+- JAVASCRIPT
+- JQUERY
+---
+
+---
+
+## _Features_
+* Navigation bar with links to various sections of the website.
+* Header section with an introduction to the developer.
+* About section showcasing Skills, Experience, and Education.
+* portfolio section to show the developers projects
+* Contact section to reach out the developer.
+
+---
+## Challenges faced during development
+Challenges i have faced as a beginner software developer,
+
+* Designing 
+* choosing the right technologies.
+* making the portfolio responsive especialy applying jQuery and JavaScript
+
+# contribution
+It is greatly appreciated if any one want to contribut and make this portfolio better. 
--- script.js
+++ script.js
@@ -1,48 +1,69 @@
-const tablinks = document.getElementsByClassName("tab-links");
-const tabcontents = document.getElementsByClassName("tab-contents");
+//enabling hidden Skill, Experience ,Education
+// const tablinks = document.getElementsByClassName("tab-links");
+// const tabcontents = document.getElementsByClassName("tab-contents");
+// function opentab(tabname) {
+//   for (tablink of tablinks) {
+//     tablink.classList.remove("active-link");
+//   }
+//   for (tabcontent of tabcontents) {
+//     tabcontent.classList.remove("active-tab");
+//   }
+//   event.currentTarget.classList.add("active-link");
+//   document.getElementById(tabname).classList.add("active-tab");
+// }
 
+  const tablinks = $(".tab-links");
+  const tabcontents = $(".tab-contents");
 
-// side menu
-// $(() => {});
+  function opentab(tabname) {
+    tablinks.removeClass("active-link");
+    tabcontents.removeClass("active-tab");
+    // activates the selected tab
+    $(event.currentTarget).addClass("active-link");
+    $("#" + tabname).addClass("active-tab");
+  }
 
-
-
-
-
-
-  // let sidemenu = document.getElementById('sidemenu');
-  // function openmenu() {
-  //     sidemenu.style.right = '0';
-
-  // }
-  // function closemenu() {
-  //     sidemenu.style.right = '-250px';
-      
-  // }
-
-
-
-
-
-
-//<script>
-
-// let openBtn = document.querySelector(".fa-xmark");
-// let closeBtn = document.querySelector(".fa-xmark");
-// let sidemenu = document.querySelector(".sidemenu");
-
-// openBtn.addEventListener('onclick', function(){
-//     sidemenu.style.display = 'block';
-// });
-// closeBtn.addEventListener('onclick', function(){
-//     sidemenu.style.display = '-none';
+//enable hidden nav bar
+// const nav = document.querySelector(".sticky-nav");
+// let lastScrollY = window.scrollY;
+// window.addEventListener("scroll", () => {
+//   //comparring last scrollY with current scrollY
+//   if (lastScrollY < window.scrollY) {
+//     nav.classList.add("sticky-nav");
+//   } else {
+//     nav.classList.remove("sticky-nav");
+//   }
+//   lastScrollY = window.scrollY;
 // });
 
+//enanbling hidden nav bar
+  const nav = $(".sticky-nav");
+  let lastScrollY = $(window).scrollTop();
+  $(window).scroll(function () {
+    if (lastScrollY < $(window).scrollTop()) {
+      nav.addClass("sticky-nav");
+    } else {
+      nav.removeClass("sticky-nav");
+    }
+    lastScrollY = $(window).scrollTop();
+  });
 
+//side menu
+// let sidemenu = document.getElementById("sidemenu");
+// function openmenu() {
+//   sidemenu.style.right = "0";
+// }
+// function closemenu() {
+//   sidemenu.style.right = "-250px";
+// }
 
-// </script>
+//side menu jquery
+let sidemenu = $("#sidemenu");
+function openmenu() {
+  sidemenu.css("right", "0");
+}
+function closemenu() {
+  sidemenu.css("right", "-250px");
+}
 
-
-
-
-
+// script to run the form sheet
